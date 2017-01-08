23f2d15e5ee9ea440ac7cea71ec5b8cb3f1ad9f2
Alex <Alex>
2017-01-08 21:16:28 +0300

    commment

---
+++ frontend/components/Bot/DeleteTaskBtn.js
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+import { Button } from 'react-bootstrap';
+import { Glyphicon } from 'react-bootstrap';
+import { deleteTask } from '../../actions/bot_queue';
+
+class DeleteTaskBtn extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onClickAction = this.onClickAction.bind(this);
+  }
+
+  onClickAction() {
+    console.log(14234, 'delete clck')
+    this.props.dispatch(deleteTask(this.props.task));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button className='delete-chapter-level-btn' onClick={this.onClickAction}>
+          <Glyphicon glyph='remove' className='delete-glyph'/>
+        </Button>
+      </div>
+    );
+  }
+}
+
+DeleteTaskBtn.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  style: PropTypes.object
+};
+
+const mapStateToProps = () => ({});
+
+export default connect(mapStateToProps)(DeleteTaskBtn);
